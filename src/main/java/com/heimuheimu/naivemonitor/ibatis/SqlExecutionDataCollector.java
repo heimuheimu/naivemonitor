2d56a0c490a25df7f6912f2823297308804754fc
heimuheimu <boyszz2009@163.com>
2017-09-05 22:12:55 +0800

    init

---
+++ src/main/java/com/heimuheimu/naivemonitor/ibatis/SqlExecutionDataCollector.java
@@ -0,0 +1,127 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 heimuheimu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.heimuheimu.naivemonitor.ibatis;
+
+import com.heimuheimu.naivemonitor.falcon.FalconData;
+import com.heimuheimu.naivemonitor.falcon.support.AbstractExecutionDataCollector;
+import com.heimuheimu.naivemonitor.monitor.ExecutionMonitor;
+import com.heimuheimu.naivemonitor.monitor.SqlExecutionMonitor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SQL 语句执行信息采集器
+ *
+ * @author heimuheimu
+ */
+public class SqlExecutionDataCollector extends AbstractExecutionDataCollector {
+
+    private static final Map<Integer, String> ERROR_METRIC_SUFFIX_MAP;
+
+    static {
+        ERROR_METRIC_SUFFIX_MAP = new HashMap<>();
+        ERROR_METRIC_SUFFIX_MAP.put(SqlExecutionMonitorFactory.ERROR_CODE_SQL_ERROR, "_error");
+        ERROR_METRIC_SUFFIX_MAP.put(SqlExecutionMonitorFactory.ERROR_CODE_SLOW_EXECUTION, "_slow_execution");
+    }
+
+    /**
+     * 当前监控数据所在的模块名称
+     */
+    private final String moduleName;
+
+    /**
+     * 当前采集器名称
+     */
+    private final String collectorName;
+
+
+    /**
+     * SQL 语句执行信息采集器
+     */
+    private final SqlExecutionMonitor sqlExecutionMonitor;
+
+    private final List<ExecutionMonitor> executionMonitorList;
+
+    /**
+     * 构造一个 SQL 语句执行信息采集器
+     *
+     * @param moduleName 模块名称
+     * @param dbName 需要采集信息的数据库名称
+     */
+    public SqlExecutionDataCollector(String moduleName, String dbName) {
+        this.moduleName = moduleName;
+        this.collectorName = dbName + "_sql";
+        this.sqlExecutionMonitor = SqlExecutionMonitorFactory.get(dbName);
+        executionMonitorList = new ArrayList<>();
+        executionMonitorList.add(sqlExecutionMonitor.getExecutionMonitor());
+    }
+
+
+    @Override
+    public int getPeriod() {
+        return 15;
+    }
+
+    @Override
+    public List<FalconData> getList() {
+        List<FalconData> falconDataList = new ArrayList<>();
+        falconDataList.addAll(super.getList());
+
+        falconDataList.add(create("_max_result_size", sqlExecutionMonitor.getMaxResultSize()));
+        sqlExecutionMonitor.resetMaxResultSize();
+
+        falconDataList.add(create("_max_updated_rows", sqlExecutionMonitor.getMaxUpdatedRows()));
+        sqlExecutionMonitor.resetMaxUpdatedRows();
+
+        falconDataList.add(create("_max_deleted_rows", sqlExecutionMonitor.getMaxDeletedRows()));
+        sqlExecutionMonitor.resetMaxDeletedRows();
+
+        return falconDataList;
+    }
+
+    @Override
+    protected List<ExecutionMonitor> getExecutionMonitorList() {
+        return executionMonitorList;
+    }
+
+    @Override
+    protected String getModuleName() {
+        return moduleName;
+    }
+
+    @Override
+    protected String getCollectorName() {
+        return collectorName;
+    }
+
+    @Override
+    protected Map<Integer, String> getErrorMetricSuffixMap() {
+        return ERROR_METRIC_SUFFIX_MAP;
+    }
+
+}
---
+++ src/main/java/com/heimuheimu/naivemonitor/ibatis/SqlExecutionMonitorFactory.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 heimuheimu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.heimuheimu.naivemonitor.ibatis;
+
+import com.heimuheimu.naivemonitor.monitor.SqlExecutionMonitor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * SQL 语句执行信息监控器工厂类
+ * <p>当前实现是线程安全的</p>
+ *
+ * @author heimuheimu
+ * @ThreadSafe
+ */
+public class SqlExecutionMonitorFactory {
+
+    private SqlExecutionMonitorFactory() {
+        //prevent construct this class
+    }
+
+    public static final int ERROR_CODE_SQL_ERROR = -1;
+
+    public static final int ERROR_CODE_SLOW_EXECUTION = -2;
+
+    private static final ConcurrentHashMap<String, SqlExecutionMonitor> SQL_EXECUTION_MONITOR_MAP = new ConcurrentHashMap<>();
+
+    private static final Object lock = new Object();
+
+    /**
+     * 根据数据库名称获得对应的 SQL 语句执行信息监控器，该方法不会返回 {@code null}
+     *
+     * @param dbName 数据库名称
+     * @return 数据库名称获得对应的 SQL 语句执行信息监控器，该方法不会返回 {@code null}
+     */
+    public static SqlExecutionMonitor get(String dbName) {
+        SqlExecutionMonitor monitor = SQL_EXECUTION_MONITOR_MAP.get(dbName);
+        if (monitor == null) {
+            synchronized (lock) {
+                monitor = SQL_EXECUTION_MONITOR_MAP.get(dbName);
+                if (monitor == null) {
+                    monitor = new SqlExecutionMonitor();
+                    SQL_EXECUTION_MONITOR_MAP.put(dbName, monitor);
+                }
+            }
+        }
+        return monitor;
+    }
+
+    /**
+     * 获得当前 SQL 语句执行信息监控器工厂类所管理的所有 SQL 语句执行信息监控器列表
+     *
+     * @return 所有 SQL 语句执行信息监控器列表
+     */
+    public static List<SqlExecutionMonitor> getAll() {
+        return new ArrayList<>(SQL_EXECUTION_MONITOR_MAP.values());
+    }
+
+}
