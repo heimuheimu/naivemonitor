7d28f2d828cc93aabe2174fa4ed071e854b11e5d
heimuheimu <boyszz2009@163.com>
2019-02-21 20:05:07 +0800

    add some monitor factories.

--- src/main/java/com/heimuheimu/naivemonitor/monitor/factory/NaiveCompressionMonitorFactory.java
+++ src/main/java/com/heimuheimu/naivemonitor/monitor/factory/NaiveCompressionMonitorFactory.java
@@ -24,43 +24,43 @@
 
 package com.heimuheimu.naivemonitor.monitor.factory;
 
-import com.heimuheimu.naivemonitor.monitor.ExecutionMonitor;
+import com.heimuheimu.naivemonitor.monitor.CompressionMonitor;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * {@link ExecutionMonitor} 工厂类。
+ * {@link CompressionMonitor} 工厂类。
  *
- * <p><strong>说明：</strong>{@code NaiveExecutionMonitorFactory} 类是线程安全的，可在多个线程中使用同一个实例。</p>
+ * <p><strong>说明：</strong>{@code NaiveCompressionMonitorFactory} 类是线程安全的，可在多个线程中使用同一个实例。</p>
  *
  * @author heimuheimu
  */
-public class NaiveExecutionMonitorFactory {
+public class NaiveCompressionMonitorFactory {
 
-    private NaiveExecutionMonitorFactory() {
+    private NaiveCompressionMonitorFactory() {
         //private constructor
     }
 
-    private static final ConcurrentHashMap<String, ExecutionMonitor> EXECUTION_MONITOR_MAP = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, CompressionMonitor> COMPRESSION_MONITOR_MAP = new ConcurrentHashMap<>();
 
     private static final Object lock = new Object();
 
     /**
-     * 根据名称获得对应的 {@code ExecutionMonitor} 实例，相同名称将返回同一个{@code ExecutionMonitor} 实例，该方法不会返回 {@code null}。
+     * 根据名称获得对应的 {@code CompressionMonitor} 实例，相同名称将返回同一个{@code CompressionMonitor} 实例，该方法不会返回 {@code null}。
      *
      * @param name 监控器名称，不允许为 {@code null}
-     * @return {@code ExecutionMonitor} 实例，不会返回 {@code null}
+     * @return {@code CompressionMonitor} 实例，不会返回 {@code null}
      */
-    public static ExecutionMonitor get(String name) {
-        ExecutionMonitor monitor = EXECUTION_MONITOR_MAP.get(name);
+    public static CompressionMonitor get(String name) {
+        CompressionMonitor monitor = COMPRESSION_MONITOR_MAP.get(name);
         if (monitor == null) {
             synchronized (lock) {
-                monitor = EXECUTION_MONITOR_MAP.get(name);
+                monitor = COMPRESSION_MONITOR_MAP.get(name);
                 if (monitor == null) {
-                    monitor = new ExecutionMonitor();
-                    EXECUTION_MONITOR_MAP.put(name, monitor);
+                    monitor = new CompressionMonitor();
+                    COMPRESSION_MONITOR_MAP.put(name, monitor);
                 }
             }
         }
@@ -68,11 +68,11 @@ public class NaiveExecutionMonitorFactory {
     }
 
     /**
-     * 获得当前 ExecutionMonitor 工厂管理的所有 ExecutionMonitor 实例列表，该方法不会返回 {@code null}。
+     * 获得当前 CompressionMonitor 工厂管理的所有 CompressionMonitor 实例列表，该方法不会返回 {@code null}。
      *
-     * @return 当前 ExecutionMonitor 工厂管理的所有 ExecutionMonitor 实例列表
+     * @return 当前 CompressionMonitor 工厂管理的所有 CompressionMonitor 实例列表
      */
-    public static List<ExecutionMonitor> getAll() {
-        return new ArrayList<>(EXECUTION_MONITOR_MAP.values());
+    public static List<CompressionMonitor> getAll() {
+        return new ArrayList<>(COMPRESSION_MONITOR_MAP.values());
     }
 }
