48e090576f70d0f1d9c75f0dd9e0f0996d9454cd
heimuheimu <boyszz2009@163.com>
2020-03-10 02:50:50 +0800

    [future] add AbstractExecutionPrometheusCollector、AbstractSocketPrometheusCollector.

---
+++ src/main/java/com/heimuheimu/naivemonitor/prometheus/support/AbstractExecutionPrometheusCollector.java
@@ -0,0 +1,130 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 heimuheimu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.heimuheimu.naivemonitor.prometheus.support;
+
+import com.heimuheimu.naivemonitor.monitor.ExecutionMonitor;
+import com.heimuheimu.naivemonitor.prometheus.PrometheusCollector;
+import com.heimuheimu.naivemonitor.prometheus.PrometheusData;
+import com.heimuheimu.naivemonitor.prometheus.PrometheusSample;
+import com.heimuheimu.naivemonitor.util.DeltaCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 操作执行信息采集器，采集时会返回以下数据：
+ * <ul>
+ *     <li>${metricPrefix}_exec_count 相邻两次采集周期内操作执行次数</li>
+ *     <li>${metricPrefix}_exec_peak_tps_count 相邻两次采集周期内每秒最大执行次数</li>
+ *     <li>${metricPrefix}_avg_exec_time_millisecond 相邻两次采集周期内单次操作平均执行时间，单位：毫秒</li>
+ *     <li>${metricPrefix}_max_exec_time_millisecond 相邻两次采集周期内单次操作最大执行时间，单位：毫秒</li>
+ * </ul>
+ *
+ * @author heimuheimu
+ * @since 1.1
+ */
+public abstract class AbstractExecutionPrometheusCollector implements PrometheusCollector {
+
+    /**
+     * 差值计算器
+     */
+    protected final DeltaCalculator deltaCalculator = new DeltaCalculator();
+
+    /**
+     * 获得操作执行相关信息 Metric 前缀。
+     *
+     * @return Metric 前缀
+     */
+    protected abstract String getMetricPrefix();
+
+    /**
+     * 获得当前采集器使用的操作执行信息监控器列表，不允许返回 {@code null} 或空。
+     *
+     * @return 操作执行信息监控器列表
+     */
+    protected abstract List<ExecutionMonitor> getMonitorList();
+
+    /**
+     * 当添加完成一个样本数据后，将会调用此方法进行回调，通常用于给样本数据添加 Label。
+     *
+     * @param monitorIndex 监控器索引
+     * @param data Prometheus 监控指标
+     * @param sample Prometheus 监控指标样本数
+     */
+    protected abstract void afterAddSample(int monitorIndex, PrometheusData data, PrometheusSample sample);
+
+    @Override
+    public List<PrometheusData> getList() {
+        List<ExecutionMonitor> monitorList = getMonitorList();
+        if (monitorList == null || monitorList.isEmpty()) {
+            throw new IllegalArgumentException("Fails to collector prometheus data: `monitorList could not be null or empty`.");
+        }
+        String metricPrefix = getMetricPrefix();
+        PrometheusData executionCountData = PrometheusData.buildGauge(metricPrefix + "_exec_count", "");
+        PrometheusData peakTpsCountData = PrometheusData.buildGauge(metricPrefix + "_exec_peak_tps_count", "");
+        PrometheusData averageExecutionTimeMillisecondData = PrometheusData.buildGauge(metricPrefix + "_avg_exec_time_millisecond", "");
+        PrometheusData maxExecutionTimeMillisecondData = PrometheusData.buildGauge(metricPrefix + "_max_exec_time_millisecond", "");
+        for (int i = 0; i < monitorList.size(); i++) {
+            ExecutionMonitor monitor = monitorList.get(i);
+            // add ${metricPrefix}_exec_count sample
+            double executionCount = deltaCalculator.delta("executionCount", monitor.getTotalCount());
+            PrometheusSample executionCountSample = PrometheusSample.build(executionCount);
+            executionCountData.addSample(executionCountSample);
+            afterAddSample(i, executionCountData, executionCountSample);
+
+            // add ${metricPrefix}_exec_peak_tps_count sample
+            PrometheusSample peakTpsSample = PrometheusSample.build(monitor.getPeakTps());
+            monitor.resetPeakTps();
+            peakTpsCountData.addSample(peakTpsSample);
+            afterAddSample(i, peakTpsCountData, peakTpsSample);
+
+            // add ${metricPrefix}_avg_exec_time_millisecond sample
+            double averageExecutionTimeMillisecond = 0d;
+            if (executionCount > 0) {
+                double executionTimeNanosecond = deltaCalculator.delta("executionTime", monitor.getTotalExecutionTime());
+                averageExecutionTimeMillisecond = executionTimeNanosecond / executionCount / 1000000d;
+            }
+            PrometheusSample averageExecutionTimeMillisecondSample = PrometheusSample.build(averageExecutionTimeMillisecond);
+            averageExecutionTimeMillisecondData.addSample(averageExecutionTimeMillisecondSample);
+            afterAddSample(i, averageExecutionTimeMillisecondData, averageExecutionTimeMillisecondSample);
+
+            // add ${metricPrefix}_max_exec_time_millisecond sample
+            double maxExecutionTimeMillisecond = 0d;
+            if (executionCount > 0) {
+                maxExecutionTimeMillisecond =  monitor.getMaxExecutionTime() / 1000000d;
+                monitor.resetMaxExecutionTime();
+            }
+            PrometheusSample maxExecutionTimeMillisecondSample = PrometheusSample.build(maxExecutionTimeMillisecond);
+            maxExecutionTimeMillisecondData.addSample(maxExecutionTimeMillisecondSample);
+            afterAddSample(i, maxExecutionTimeMillisecondData, maxExecutionTimeMillisecondSample);
+        }
+        List<PrometheusData> dataList = new ArrayList<>();
+        dataList.add(executionCountData);
+        dataList.add(peakTpsCountData);
+        dataList.add(averageExecutionTimeMillisecondData);
+        dataList.add(maxExecutionTimeMillisecondData);
+        return dataList;
+    }
+}
---
+++ src/main/java/com/heimuheimu/naivemonitor/prometheus/support/AbstractSocketPrometheusCollector.java
@@ -0,0 +1,142 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 heimuheimu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.heimuheimu.naivemonitor.prometheus.support;
+
+import com.heimuheimu.naivemonitor.monitor.SocketMonitor;
+import com.heimuheimu.naivemonitor.prometheus.PrometheusCollector;
+import com.heimuheimu.naivemonitor.prometheus.PrometheusData;
+import com.heimuheimu.naivemonitor.prometheus.PrometheusSample;
+import com.heimuheimu.naivemonitor.util.DeltaCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Socket 读、写信息采集器，采集时会返回以下数据：
+ * <ul>
+ *     <li>${metricPrefix}_socket_read_count{remoteAddress="$remoteAddress"} 相邻两次采集周期内 Socket 读取的次数</li>
+ *     <li>${metricPrefix}_socket_read_bytes{remoteAddress="$remoteAddress"} 相邻两次采集周期内 Socket 读取的字节总数</li>
+ *     <li>${metricPrefix}_socket_max_read_bytes{remoteAddress="$remoteAddress"} 相邻两次采集周期内单次 Socket 读取的最大字节数</li>
+ *     <li>${metricPrefix}_socket_write_count{remoteAddress="$remoteAddress"} 相邻两次采集周期内 Socket 写入的次数</li>
+ *     <li>${metricPrefix}_socket_write_bytes{remoteAddress="$remoteAddress"} 相邻两次采集周期内 Socket 写入的字节总数</li>
+ *     <li>${metricPrefix}_socket_max_write_bytes{remoteAddress="$remoteAddress"} 相邻两次采集周期内单次 Socket 写入的最大字节数</li>
+ * </ul>
+ *
+ * @author heimuheimu
+ * @since 1.1
+ */
+public abstract class AbstractSocketPrometheusCollector implements PrometheusCollector {
+
+    /**
+     * 差值计算器
+     */
+    protected final DeltaCalculator deltaCalculator = new DeltaCalculator();
+
+    /**
+     * 获得 Socket 读、写信息 Metric 前缀。
+     *
+     * @return Metric 前缀
+     */
+    protected abstract String getMetricPrefix();
+
+    /**
+     * 获得当前采集器使用的 Socket 读、写信息监控器列表，不允许返回 {@code null} 或空。
+     *
+     * @return Socket 读、写信息监控器列表
+     */
+    protected abstract List<SocketMonitor> getMonitorList();
+
+    /**
+     * 当添加完成一个样本数据后，将会调用此方法进行回调，通常用于给样本数据添加 Label。
+     *
+     * @param monitorIndex 监控器索引
+     * @param data Prometheus 监控指标
+     * @param sample Prometheus 监控指标样本数
+     */
+    protected abstract void afterAddSample(int monitorIndex, PrometheusData data, PrometheusSample sample);
+
+    @Override
+    public List<PrometheusData> getList() {
+        List<SocketMonitor> monitorList = getMonitorList();
+        if (monitorList == null || monitorList.isEmpty()) {
+            throw new IllegalArgumentException("Fails to collector prometheus data: `monitorList could not be null or empty`.");
+        }
+        String metricPrefix = getMetricPrefix();
+        PrometheusData readCountData = PrometheusData.buildGauge(metricPrefix + "_socket_read_count", "");
+        PrometheusData readBytesData = PrometheusData.buildGauge(metricPrefix + "_socket_read_bytes", "");
+        PrometheusData maxReadBytesData = PrometheusData.buildGauge(metricPrefix + "_socket_max_read_bytes", "");
+        PrometheusData writeCountData = PrometheusData.buildGauge(metricPrefix + "_socket_write_count", "");
+        PrometheusData writeBytesData = PrometheusData.buildGauge(metricPrefix + "_socket_write_bytes", "");
+        PrometheusData maxWriteBytesData = PrometheusData.buildGauge(metricPrefix + "_socket_max_write_bytes", "");
+        for (int i = 0; i < monitorList.size(); i++) {
+            SocketMonitor monitor = monitorList.get(i);
+            // add ${metricPrefix}_socket_read_count sample
+            PrometheusSample readCountSample = PrometheusSample.build(deltaCalculator.delta("readCount", monitor.getReadCount()))
+                    .addSampleLabel("remoteAddress", monitor.getHost());
+            readCountData.addSample(readCountSample);
+            afterAddSample(i, readCountData, readCountSample);
+
+            // add ${metricPrefix}_socket_read_bytes sample
+            PrometheusSample readBytesSample = PrometheusSample.build(deltaCalculator.delta("readBytes", monitor.getReadByteCount()))
+                    .addSampleLabel("remoteAddress", monitor.getHost());
+            readBytesData.addSample(readBytesSample);
+            afterAddSample(i, readBytesData, readBytesSample);
+
+            // add ${metricPrefix}_socket_max_read_bytes sample
+            PrometheusSample maxReadBytesSample = PrometheusSample.build(monitor.getMaxReadByteCount())
+                    .addSampleLabel("remoteAddress", monitor.getHost());
+            monitor.resetMaxReadByteCount();
+            maxReadBytesData.addSample(maxReadBytesSample);
+            afterAddSample(i, maxReadBytesData, maxReadBytesSample);
+
+            // add ${metricPrefix}_socket_write_count sample
+            PrometheusSample writeCountSample = PrometheusSample.build(deltaCalculator.delta("writeCount", monitor.getWrittenCount()))
+                    .addSampleLabel("remoteAddress", monitor.getHost());
+            writeCountData.addSample(writeCountSample);
+            afterAddSample(i, writeCountData, writeCountSample);
+
+            // add ${metricPrefix}_socket_write_bytes sample
+            PrometheusSample writeBytesSample = PrometheusSample.build(deltaCalculator.delta("writeBytes", monitor.getWrittenByteCount()))
+                    .addSampleLabel("remoteAddress", monitor.getHost());
+            writeBytesData.addSample(writeBytesSample);
+            afterAddSample(i, writeBytesData, writeBytesSample);
+
+            // add ${metricPrefix}_socket_max_write_bytes
+            PrometheusSample maxWriteBytesSample = PrometheusSample.build(monitor.getMaxWrittenByteCount())
+                    .addSampleLabel("remoteAddress", monitor.getHost());
+            monitor.resetMaxWrittenByteCount();
+            maxWriteBytesData.addSample(maxWriteBytesSample);
+            afterAddSample(i, maxWriteBytesData, maxWriteBytesSample);
+        }
+        List<PrometheusData> dataList = new ArrayList<>();
+        dataList.add(readCountData);
+        dataList.add(readBytesData);
+        dataList.add(maxReadBytesData);
+        dataList.add(writeCountData);
+        dataList.add(writeBytesData);
+        dataList.add(maxWriteBytesData);
+        return dataList;
+    }
+}
